41e731a0849883f4805a589d7492a820b02140f4
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-02-16 16:51:46 +0100

    created Flat model, pulled Bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Beautiful Stylish & Cosy Victorian Apartment',
+  address: '90 Meadowbank Park',
+  description: "My lovely apartment is on the ground floor and is newly renovated,",
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Entire rental unit hosted by Krste',
+  address: 'Praha 1, Hlavní město Praha, Czechia',
+  description: "We would like to invite you in our Royal apartment a few steps from the Charles Bridge.",
+  price_per_night: 90,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Explore Prague from the Old Town Square',
+  address: 'Hlavní město Praha, Czechia',
+  description: "Cross a warm parquet floor to a kitchen that's a study in modernism and minimalism.",
+  price_per_night: 40,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'City centre apartment in old town Edinburgh',
+  address: '4 Bread str, Edinburgh, Scotland, United Kingdom',
+  description: "Basement flat. Everything within 5 minutes walking distance. Castle, Royal mile, old town, Princes st.",
+  price_per_night: 40,
+  number_of_guests: 3
+)
